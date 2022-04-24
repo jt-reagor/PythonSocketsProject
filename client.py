afd7918ad61c32419324580be382e3c05dd000ca
Rainstorm0 <53408757+Rainstorm0@users.noreply.github.com>
2022-04-24 14:38:42 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
-import os
 import socket
 import time
+import matplotlib.pyplot as plt
+import math
 
 START = "<START>"
 SPLIT = "<SPLIT>"
@@ -21,6 +22,76 @@ def receive(client):
         return -1
     data_back_split = data_back_split[1:]
     return data_back_split
+
+
+def smooth(data, smooth_factor):
+    smoothed_data = []
+    for n in range(len(data) - smooth_factor):
+        sum = 0
+        highest = 0
+        for k in range(smooth_factor):
+            highest = max(highest, data[n + k])
+            sum += data[n + k]
+        smoothed_data.append(highest)
+        # smoothed_data.append(sum / smooth_factor)
+
+    return smoothed_data
+
+
+def upload_plot():
+    speeds = []
+    times = []
+    with open("upload_speed.txt") as file:
+        for line in file:
+            split_line = line.split()
+            speeds.append(float(split_line[0]) / (10 ** 6))
+            times.append(float(split_line[1]))
+
+    smooth_speeds = smooth(speeds, 10)
+    smooth_times = smooth(times, 10)
+
+    # print(times)
+    # print(speeds)
+    # plt.scatter(times, speeds, s=1)
+    # plt.show()
+    fig, ax = plt.subplots()
+    ax.plot(smooth_times, smooth_speeds, lw=1)
+    scale = 1
+    while 10 * scale < math.ceil(max(smooth_times)):
+        scale *= 10
+    plt.xticks(range(0, math.ceil(max(smooth_times)) + 1, scale))
+    plt.title("Upload Speed vs. Time")
+    plt.xlabel("Time (sec)")
+    plt.ylabel("Speed (MB/sec)")
+    plt.show()
+
+
+def download_plot():
+    speeds = []
+    times = []
+    with open("download_speed.txt") as file:
+        for line in file:
+            split_line = line.split()
+            speeds.append(float(split_line[0]) / (10 ** 6))
+            times.append(float(split_line[1]))
+
+    smooth_speeds = smooth(speeds, 10)
+    smooth_times = smooth(times, 10)
+
+    # print(times)
+    # print(speeds)
+    # plt.scatter(times, speeds, s=1)
+    # plt.show()
+    fig, ax = plt.subplots()
+    ax.plot(smooth_times, smooth_speeds, lw=0.5)
+    scale = 1
+    while 10 * scale < math.ceil(max(smooth_times)):
+        scale *= 10
+    plt.xticks(range(0, math.ceil(max(smooth_times)) + 1, scale))
+    plt.title("Download Speed vs. Time")
+    plt.xlabel("Time (sec)")
+    plt.ylabel("Speed (MB/sec)")
+    plt.show()
 
 
 def main():
@@ -84,6 +155,27 @@ def main():
                     to_print += item + "\t"
                 print(to_print)
 
+        if cmd == "MKDIR":  # make directory with the name given
+            new_dir_name = line_in_split[1]
+            data_to_send += "MKDIR" + SPLIT + new_dir_name
+            client.send(data_to_send.encode(FORMAT))
+
+        if cmd == "CD":  # change directory
+            new_dir_name = line_in_split[1]
+            data_to_send += "CD" + SPLIT + new_dir_name
+            client.send(data_to_send.encode(FORMAT))
+
+        if cmd == "DELETE":  # delete file from folder, takes one arg: filename
+            fname = line_in_split[1]
+            data_to_send += "DELETE"+SPLIT+fname
+            client.send(data_to_send.encode(FORMAT))
+            # add in error handling
+
+        if cmd == "DELDIR": # delete folder, takes one arg: directory name
+            dir_name = line_in_split[1]
+            data_to_send += "DELDIR" + SPLIT + dir_name
+            client.send(data_to_send.encode(FORMAT))
+
         if cmd == "DELETE":  # delete file from folder, takes one arg: filename
             fname = line_in_split[1]
             data_to_send += "DELETE" + SPLIT + fname
@@ -101,26 +193,53 @@ def main():
             client.send(data_to_send.encode(FORMAT))  # send "ready to send x bytes"
             data_back = receive(client)  # wait for ack from server
             # print("INITIAL READY RECIEVED")
-            print(f"Sending file {fname}")
             if data_back == -1:
                 print("ERROR: PROBLEM IN READY ACK")
                 continue
+            file = open("upload_speed.txt", "w")  #
+            file.close()  #
+            base_time = time.time_ns()  #
+            num_bytes = 0  #
+            file = open("upload_speed.txt", "a")  #
+            print_time = time.time()  #
+            print_bytes = 0  #
+            total_bytes = 0
             while f.tell() < length:
                 buff = f.read(BUFFLEN)
                 # print(f"Sending {buff}")
+                start_time = time.time_ns()  #
                 client.send(buff)  # send buffer of data
                 # print("Buffer sent")
                 # print("Waiting for ready ack")
                 data_back = receive(client)  # wait for server to be ready for next
+                end_time = time.time_ns()  #
                 if data_back[0] != "READY":
                     print("ERROR: DID NOT RECEIVE READY ACK")
                     return
                 else:
+                    num_bytes += len(buff)  #
+                    print_bytes += len(buff)  #
+                    total_bytes += len(buff)  #
+                    if end_time > start_time:  #
+                        time_passed = end_time - start_time  #
+                        speed = num_bytes / (time_passed / (10 ** 9))  #
+                        current_time = end_time - base_time  #
+                        file.write(str(speed) + " " + str(current_time / (10 ** 9)) + "\n")  #
+                        num_bytes = 0  #
+                    if time.time() >= print_time + 1:  #
+                        print_speed = print_bytes / (time.time() - print_time)  #
+                        print_time = time.time()  #
+                        print_bytes = 0  #
+                        percent_done = round(100 * total_bytes / length)
+                        print("\r" + str(round(print_speed / (10 ** 6), 3)) + " MB/sec - " + str(percent_done) + "%",
+                              end="")  #
                     # print("Ready received")
                     pass
+            file.close()
+            print()
             end_message = START + SPLIT + "DONE"
             client.send(end_message.encode(FORMAT))  # tell server finished sending
-            print(f"File sent")
+            print("File uploaded")
 
             f.close()
 
@@ -139,15 +258,43 @@ def main():
             send_data += "READY"
             client.send(send_data.encode(FORMAT))
             buff = bytes()
+            file = open("download_speed.txt", "w")  #
+            file.close()  #
+            base_time = time.time_ns()  #
+            start_time = 1000000000  #
+            num_bytes = 0  #
+            file = open("download_speed.txt", "a")  #
+            print_time = time.time()  #
+            print_bytes = 0
             while len(buff) < data_length:
                 data_in = client.recv(BUFFLEN)
+                end_time = time.time_ns()  #
+                num_bytes += len(data_in)  #
+                print_bytes += len(data_in)  #
+                if end_time > start_time:  #
+                    time_passed = end_time - start_time  #
+                    speed = num_bytes / (time_passed / (10 ** 9))  #
+                    current_time = end_time - base_time  #
+                    file.write(str(speed) + " " + str(current_time / (10 ** 9)) + "\n")  #
+                    num_bytes = 0  #
+                if time.time() >= print_time + 1:  #
+                    print_speed = print_bytes / (time.time() - print_time)  #
+                    print_time = time.time()  #
+                    print_bytes = 0  #
+                    percent_done = round(100 * len(buff) / data_length)
+                    print("\r" + str(round(print_speed / (10 ** 6), 3)) + " MB/sec - " + str(percent_done) + "%",
+                          end="")  #
                 buff += data_in
                 # print(buff)
                 send_data = START + SPLIT + "READY"  # send ready ack
+                if end_time > start_time:  #
+                    start_time = time.time_ns()  #
                 client.send(send_data.encode(FORMAT))
+            file.close()  #
             f.write(buff)  # write bytes from buffer to file
             data_in = receive(client)  # get acknowledgement that client has finished
             if data_in[0] == "DONE":
+                print()
                 print("Done receiving")
             else:
                 print("ERROR: DID NOT RECEIVE DONE")
